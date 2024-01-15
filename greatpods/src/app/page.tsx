63d03d224e873edef2054a6a89ca76cf5629bd4a
Brian Lee <bnlee419@gmail.com>
2024-01-15 14:41:43 -0800

    hello

---
+++ greatpods/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+      OK
+    </div>
+  );
+}
